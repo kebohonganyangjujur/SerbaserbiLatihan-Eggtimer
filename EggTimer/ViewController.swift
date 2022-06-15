2cfafbbf56a4303c541abc4b46e94d11bad05475
kebohonganyangjujur <kebohonganyangjujur@gmail.com>
2022-06-15 23:26:46 +0700

    Apply scheduledTimer, now app can do countdown

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,24 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let softTime = 3
-    let mediumTime = 5
-    let hardTime = 7
-    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var secondsRemaining = 60
+    var timer = Timer()
+   
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
-     
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium" :
-            print(mediumTime)
-        case "Hard" :
-            print(hardTime)
-        default:
-            print("Error")
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
         }
     }
-    
 
 }
